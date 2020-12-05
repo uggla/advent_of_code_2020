439be3d19e62f5066275527cbe9c1cebe3f9c2ab
Uggla <uggla@free.fr>
2020-12-05 03:01:22 +0100

    Solution 2 in progress

---
+++ four/solution2/src/main.rs
@@ -0,0 +1,273 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::fmt::Write;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[allow(dead_code)]
+fn get_passport(n: usize, passports: &Vec<HashMap<String, String>>) -> String {
+    let mut key_values: Vec<String> = Vec::new();
+    for (key, value) in &passports[n] {
+        let mut key_value = String::new();
+        write!(&mut key_value, "{}:{}", key, value).unwrap();
+        key_values.push(key_value.clone());
+    }
+    key_values.sort();
+    key_values.join(" ")
+}
+
+fn parse(path: &str) -> Vec<HashMap<String, String>> {
+    let mut passports: Vec<HashMap<String, String>> = Vec::new();
+    // Parse
+    let input = File::open(path).expect("File error");
+    let buffered = BufReader::new(input);
+
+    let mut passport: HashMap<String, String> = HashMap::new();
+    for line in buffered.lines() {
+        let linestr = line.unwrap();
+        // println!("{}", line.unwrap());
+        if linestr.is_empty() {
+            passports.push(passport.clone());
+            passport = HashMap::new();
+        } else {
+            let key_values: Vec<&str> = linestr.split(" ").collect();
+            for key_value in key_values {
+                let item: Vec<&str> = key_value.split(":").collect();
+                passport.insert(item[0].to_string(), item[1].to_string());
+            }
+        }
+    }
+    passports
+}
+
+fn check_year_range(value: &String, min: usize, max: usize) -> bool {
+    let re = Regex::new(r"^\d{4}$").unwrap();
+    if re.is_match(value) {
+        let year: usize = value.parse().unwrap();
+        println!("year {}", year);
+        if year <= max && year >= min {
+            return true;
+        }
+    }
+    false
+}
+
+fn check_byr(value: &String) -> bool {
+    check_year_range(value, 1920, 2002)
+}
+
+fn check_iyr(value: &String) -> bool {
+    check_year_range(value, 2010, 2020)
+}
+
+fn check_eyr(value: &String) -> bool {
+    check_year_range(value, 2020, 2030)
+}
+
+fn check_passport(n: usize, passports: &Vec<HashMap<String, String>>) -> bool {
+    match passports[n].get("byr") {
+        Some(value) => {
+            if !check_byr(value) {
+                return false;
+            }
+        }
+        None => return false,
+    };
+
+    match passports[n].get("iyr") {
+        Some(value) => {
+            if !check_iyr(value) {
+                return false;
+            }
+        }
+        None => return false,
+    };
+
+    match passports[n].get("eyr") {
+        Some(value) => {
+            if !check_eyr(value) {
+                return false;
+            }
+        }
+        None => return false,
+    };
+
+    true
+}
+
+fn main() {
+    let path = "input";
+    let mut nb_valid_passports = 0;
+    let passports: Vec<HashMap<String, String>>;
+
+    passports = parse(path);
+
+    for passport in 0..passports.len() {
+        if check_passport(passport, &passports) {
+            nb_valid_passports += 1;
+        }
+    }
+
+    println!("Valid passwords: {}", nb_valid_passports);
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_fake() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_0_entry() {
+        let path = "input";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = get_passport(0, &passports);
+        assert_eq!(
+            output,
+            "byr:1940 cid:139 ecl:blu eyr:2025 hcl:#ceb3a1 hgt:159cm iyr:2017 pid:561068005"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_1_entry() {
+        let path = "input";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = get_passport(1, &passports);
+        assert_eq!(
+            output,
+            "byr:1986 ecl:hzl eyr:2025 iyr:2014 pid:960679613".to_string()
+        );
+    }
+
+    // #[test]
+    // fn test_check_passport() {
+    //     let path = "input";
+    //     let passports: Vec<HashMap<String, String>>;
+    //     passports = parse(path);
+    //     let output = check_passport(0, &passports);
+    //     assert_eq!(output, true);
+    //     let output = check_passport(1, &passports);
+    //     assert_eq!(output, true);
+    //     let output = check_passport(2, &passports);
+    //     assert_eq!(output, true);
+    //     let output = check_passport(3, &passports);
+    //     assert_eq!(output, true);
+    //     let output = check_passport(4, &passports);
+    //     assert_eq!(output, false);
+    // }
+
+    #[test]
+    fn test_check_passport_byr() {
+        let path = "input_byr_ok";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, true);
+    }
+
+    #[test]
+    fn test_check_passport_byr_ko_1() {
+        let path = "input_byr_ko_1";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_byr_ko_2() {
+        let path = "input_byr_ko_2";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_byr_ko_3() {
+        let path = "input_byr_ko_3";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_iyr() {
+        let path = "input_iyr_ok";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, true);
+    }
+
+    #[test]
+    fn test_check_passport_iyr_ko_1() {
+        let path = "input_iyr_ko_1";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_iyr_ko_2() {
+        let path = "input_iyr_ko_2";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_iyr_ko_3() {
+        let path = "input_iyr_ko_3";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_eyr() {
+        let path = "input_eyr_ok";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, true);
+    }
+
+    #[test]
+    fn test_check_passport_eyr_ko_1() {
+        let path = "input_eyr_ko_1";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_eyr_ko_2() {
+        let path = "input_eyr_ko_2";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+
+    #[test]
+    fn test_check_passport_eyr_ko_3() {
+        let path = "input_eyr_ko_3";
+        let passports: Vec<HashMap<String, String>>;
+        passports = parse(path);
+        let output = check_passport(0, &passports);
+        assert_eq!(output, false);
+    }
+}
